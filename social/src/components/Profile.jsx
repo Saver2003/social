a19bc7f62fa2af632c66bd6bed81d07d591af1b6
Aleksander Gareev <saver2003@gmail.com>
2020-02-29 13:32:33 +0600

    изменены некоторые стили

--- social/src/components/Profile.jsx
+++ social/src/components/Profile.jsx
@@ -3,10 +3,25 @@ import React from 'react';
 const Profile = () => {
   return (
     <div className="content">
-      <div>
-        <img src="https://www.guideposts.org/sites/guideposts.org/files/styles/slideshow_grid_view/public/slide1_12.jpg" alt="main theme"/>
+      <div className="image-cover">
+        <img src="https://www.guideposts.org/sites/guideposts.org/files/styles/slideshow_grid_view/public/slide1_12.jpg"
+             alt="main theme" className="main-img"/>
       </div>
-      <div>ava + description</div>
+      <div className="ava-wrapper">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaOJeQgJS1vwCJ6tj4-OAvWRl8c7IrnRRk7lKLxyBMnJVcVDl5&s"
+          alt="ava" className="avatar"/>
+        <div>
+          <div className="user-name">
+            <p>Alex G.</p>
+          </div>
+          <div className="personal-info">
+            <p>Date of Birth: 4 february</p>
+            <p>City: Bishkek</p>
+            <p>Education: Attractor school JS-2</p>
+          </div>
+        </div>
+      </div>
       <div>
         my posts
         <div>new post</div>

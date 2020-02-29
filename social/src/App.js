dc5423b28a5c361515edb0c2fc287d2be60b04e3
Aleksander Gareev <saver2003@gmail.com>
2020-02-29 12:30:15 +0600

    структура приложения разнесена по компонентам

--- social/src/App.js
+++ social/src/App.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Profile from "./components/Profile";
+
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img src='https://www.freelogodesign.org/Content/img/logo-samples/celtica.png' alt="logo"/>
-      </header>
-      <nav className="nav">
-        <div><a href="/">Profile</a></div>
-        <div><a href="/">Messages</a></div>
-        <div><a href="/">News</a></div>
-        <div><a href="/">Music</a></div>
-        <div><a href="/">Settings</a></div>
-      </nav>
-      <div className="content">
-        <div>
-          <img src="https://www.guideposts.org/sites/guideposts.org/files/styles/slideshow_grid_view/public/slide1_12.jpg" alt="main theme"/>
-        </div>
-        <div>ava + description</div>
-        <div>
-          my posts
-          <div>new post</div>
-        </div>
-        <div>post 1</div>
-        <div>post 2</div>
-      </div>
+      <Header/>
+      <NavBar/>
+      <Profile/>
     </div>
   );
 }
---
+++ social/src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.guideposts.org/sites/guideposts.org/files/styles/slideshow_grid_view/public/slide1_12.jpg" alt="main theme"/>
+      </div>
+      <div>ava + description</div>
+      <div>
+        my posts
+        <div>new post</div>
+      </div>
+      <div>post 1</div>
+      <div>post 2</div>
+    </div>
+  );
+};
+
+export default Profile;
